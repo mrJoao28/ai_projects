6c46ffca1f6846c9bc8a2fbec444e425664ddc61
mrjhon <joaofelipeazevedo28@gmail.com>
2026-02-28 10:23:29 -0400

    modified:   text-classification.py

--- text-classification.py
+++ text-classification.py
@@ -1,21 +1,42 @@
 import torch
 from transformers import DistilBertTokenizer, DistilBertForSequenceClassification
-import pandas as pd
+from transformers import T5ForConditionalGeneration, T5Tokenizer
+import pytesseract 
+import cv2
+import pyautogui
+import os
 
 
-text = str(input("put a text: "))
+model_name1 = 't5-small'
+model1 = T5ForConditionalGeneration.from_pretrained(model_name1, device_map="auto").eval()
+tokenizer1 = T5Tokenizer.from_pretrained(model_name1)
 
-                         
-tokenizer = DistilBertTokenizer.from_pretrained("distilbert-base-uncased-finetuned-sst-2-english")
-model = DistilBertForSequenceClassification.from_pretrained("distilbert-base-uncased-finetuned-sst-2-english")
+tokenizer2 = DistilBertTokenizer.from_pretrained("distilbert-base-uncased-finetuned-sst-2-english")
+model2 = DistilBertForSequenceClassification.from_pretrained("distilbert-base-uncased-finetuned-sst-2-english").eval()
+caminho = "C:\Program Files\Tesseract-OCR"
 
-inputs = tokenizer(text , return_tensors="pt")
-with torch.no_grad():
-    logits = model(**inputs).logits
-predicted_class_id = logits.argmax().item()
-model.config.id2label[predicted_class_id]
+while True:
+    img = pyautogui.screenshot()
+    img.save("imagem.png")
+    image = cv2.imread("imagem.png")
+    pytesseract.pytesseract.tesseract_cmd = caminho + r"\tesseract.exe"
+    text = pytesseract.image_to_string(image)
+    text = f"translate Portuguese to English: {text}"
+    input_ids = tokenizer1(text, return_tensors="pt").input_ids.to(model1.device)
+    outputs = model1.generate(input_ids=input_ids)
+    translate_text =  tokenizer1.decode(outputs[0], skip_special_tokens=True)
+    
+    inputs = tokenizer2(translate_text , return_tensors="pt")
+    with torch.no_grad():
+        logits = model2(**inputs).logits
+    predicted_class_id = logits.argmax().item()
+    model2.config.id2label[predicted_class_id]
 
-if predicted_class_id == 0:
-    print("Warning")
-else :
-    print("Safe")
+    if predicted_class_id == 0:
+        print("Warning")
+    else :
+        print("Safe")
+
+    os.remove("imagem.png")
+
+
